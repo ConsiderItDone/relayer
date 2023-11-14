b2f5ac7c65e80573f4234a666807ee8ddf9586e1
ramil <ramilexe@gmail.com>
2023-11-14 07:06:47 +0300

    add key test

--- relayer/chains/avalanche/config.go
+++ relayer/chains/avalanche/config.go
@@ -2,8 +2,10 @@ package avalanche
 
 import (
 	"net/url"
+	"os"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/types/module"
 	"go.uber.org/zap"
 
 	"github.com/cosmos/relayer/v2/relayer/codecs/ethermint"
@@ -13,15 +15,16 @@ import (
 var _ provider.ProviderConfig = &AvalancheProviderConfig{}
 
 type AvalancheProviderConfig struct {
-	RPCAddr         string `json:"rpc-addr" yaml:"rpc-addr"`
-	ChainID         string `json:"chain-id" yaml:"chain-id"`
-	ChainName       string `json:"-" yaml:"-"`
-	Timeout         string `json:"timeout" yaml:"timeout"`
-	ContractAddress string `json:"contract-address" yaml:"contract-address"`
-	KeyDirectory    string `json:"key-directory" yaml:"key-directory"`
-	Key             string `json:"key" yaml:"key"`
-	KeyringBackend  string `json:"keyring-backend" yaml:"keyring-backend"`
-	AccountPrefix   string `json:"account-prefix" yaml:"account-prefix"`
+	RPCAddr         string                  `json:"rpc-addr" yaml:"rpc-addr"`
+	ChainID         string                  `json:"chain-id" yaml:"chain-id"`
+	ChainName       string                  `json:"-" yaml:"-"`
+	Timeout         string                  `json:"timeout" yaml:"timeout"`
+	ContractAddress string                  `json:"contract-address" yaml:"contract-address"`
+	KeyDirectory    string                  `json:"key-directory" yaml:"key-directory"`
+	Key             string                  `json:"key" yaml:"key"`
+	KeyringBackend  string                  `json:"keyring-backend" yaml:"keyring-backend"`
+	ExtraCodecs     []string                `json:"extra-codecs" yaml:"extra-codecs"`
+	Modules         []module.AppModuleBasic `json:"-" yaml:"-"`
 }
 
 func (ac AvalancheProviderConfig) NewProvider(log *zap.Logger, homepath string, debug bool, chainName string) (provider.ChainProvider, error) {
@@ -30,10 +33,16 @@ func (ac AvalancheProviderConfig) NewProvider(log *zap.Logger, homepath string, 
 	}
 	ac.ChainName = chainName
 
+	ac.Modules = append([]module.AppModuleBasic{}, moduleBasics...)
+
 	return &AvalancheProvider{
 		log:            log,
 		PCfg:           ac,
 		KeyringOptions: []keyring.Option{ethermint.EthSecp256k1Option()},
+		Input:          os.Stdin,
+		Output:         os.Stdout,
+
+		Codec: makeCodec(ac.Modules, ac.ExtraCodecs),
 	}, nil
 }
 
