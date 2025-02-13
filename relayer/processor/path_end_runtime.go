68cf7d92597079a00f050481e71c56802799253d
Ilnur <ilnur.a.g@gmail.com>
2025-02-14 00:51:34 +0400

    modify shouldSendConnectionMessage in pathEndRuntime

--- relayer/processor/path_end_runtime.go
+++ relayer/processor/path_end_runtime.go
@@ -60,6 +60,8 @@ type pathEndRuntime struct {
 
 	finishedProcessing chan messageToTrack
 	retryCount         uint64
+
+	blockRetries map[uint64]int
 }
 
 func newPathEndRuntime(log *zap.Logger, pathEnd PathEnd, metrics *PrometheusMetrics) *pathEndRuntime {
@@ -83,6 +85,7 @@ func newPathEndRuntime(log *zap.Logger, pathEnd PathEnd, metrics *PrometheusMetr
 		clientICQProcessing:  newClientICQProcessingCache(),
 		connSubscribers:      make(map[string][]func(provider.ConnectionInfo)),
 		metrics:              metrics,
+		blockRetries:         make(map[uint64]int),
 	}
 }
 
@@ -734,11 +737,20 @@ func (pathEnd *pathEndRuntime) shouldSendChannelMessage(message channelIBCMessag
 	}
 
 	if message.info.Height >= counterparty.latestBlock.Height {
+		tries := pathEnd.blockRetries[message.info.Height]
+		stuck := false
+		if tries > 5 {
+			stuck = true
+			tries = 0
+		}
+		pathEnd.blockRetries[message.info.Height] = tries + 1
 		pathEnd.log.Debug("Waiting to relay channel message until counterparty height has incremented",
 			zap.Inline(channelKey),
 			zap.String("event_type", eventType),
 		)
-		return false
+		if !stuck {
+			return false
+		}
 	}
 	msgProcessCache, ok := pathEnd.channelProcessing[eventType]
 	if !ok {
