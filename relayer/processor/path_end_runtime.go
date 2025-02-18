610c962060d2dc66e0edc0252d031677e8558aa9
Ilnur <ilnur.a.g@gmail.com>
2025-02-18 12:08:44 +0300

    fix shouldSendPacketMessage in case when blocks didn't increment

--- relayer/processor/path_end_runtime.go
+++ relayer/processor/path_end_runtime.go
@@ -548,6 +548,13 @@ func (pathEnd *pathEndRuntime) shouldSendPacketMessage(message packetIBCMessage,
 	}
 
 	if message.info.Height >= pathEndForHeight.latestBlock.Height {
+		tries := pathEnd.blockRetries[message.info.Height]
+		stuck := false
+		if tries > 5 {
+			stuck = true
+			tries = 0
+		}
+		pathEnd.blockRetries[message.info.Height] = tries + 1
 		pathEnd.log.Debug("Waiting to relay packet message until counterparty height has incremented",
 			zap.String("event_type", eventType),
 			zap.Uint64("sequence", sequence),
@@ -555,7 +562,9 @@ func (pathEnd *pathEndRuntime) shouldSendPacketMessage(message packetIBCMessage,
 			zap.Uint64("counterparty_height", counterparty.latestBlock.Height),
 			zap.Inline(k),
 		)
-		return false
+		if !stuck {
+			return false
+		}
 	}
 	if !pathEnd.channelStateCache[k].Open {
 		// channel is not open, do not send
